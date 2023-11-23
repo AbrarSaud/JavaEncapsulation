ffbea4d416d61da0e53a6a789f2c9d088a102dac
AbrarSaud <abrar.sauod0@gmail.com>
2023-11-23 04:32:12 +0300

    add List Categories Data and model

---
+++ lib/data/categories_data.dart
@@ -0,0 +1,28 @@
+import 'package:athkar_app/model/category_data.dart';
+
+List CategoriesData = [
+  Category(
+    id: "c1",
+    title: "أذكار الصباح",
+  ),
+  Category(
+    id: "c2",
+    title: "أذكار المساء",
+  ),
+  Category(
+    id: "c3",
+    title: "أذكار النوم",
+  ),
+  Category(
+    id: "c4",
+    title: "أذكار الصلاة",
+  ),
+  Category(
+    id: "c5",
+    title: "أذكار المسجد",
+  ),
+   Category(
+    id: "c6",
+    title: "أذكار الوضوء",
+  ),
+];
